fd3f68fe1930653495b3c3051e7b12b7c5a19db9
na <707150586@qq.com>
2018-08-02 01:26:05 +0800

    2018080201

--- vipMangementSystem/vms/views.py
+++ vipMangementSystem/vms/views.py
@@ -11,27 +11,74 @@ from DB import Mysql
 
 # Create your views here.
 import json
-
+import time
 
 def list_vip_person(req):
     # print req.GET
     db = Mysql()
-    person_list =  list(db.getAll('select a.id,a.`name` as vip_name,a.phone as vip_phone,a.note as vip_notes,a.sex as vip_sex,b.point as vip_person_point from person a,point_detail b WHERE a.id = b.person_id and a.name=\'温娜\'')
-    )
+    sql = 'SELECT a.id, a.`name` AS vip_name, a.phone AS vip_phone, a.note AS vip_notes, a.sex AS vip_sex, b.point AS vip_person_point' \
+          ' FROM person a LEFT JOIN point_detail b ON a.id = b.person_id  ORDER BY a.id'
+    person_list = list(db.getAll(sql))
+    print person_list
+    print len(person_list)
     db.dispose()
 
     limit = int(req.GET['limit'])
     page = int(req.GET['page'])
 
     # person_list =  list(vip_person.objects.filter().values())
-
+    n_list = []
+    for x in person_list:
+        if x['vip_person_point'] is None:
+            x['vip_person_point'] = 0
+        if x['vip_sex'] == 0:
+            x['vip_sex'] = '女'
+        elif x['vip_sex'] == 1:
+            x['vip_sex'] = '男'
+        else:
+            x['vip_sex'] = '未知'
+        n_list.append(x)
+    print len(n_list)
+    print len(n_list[(page-1)*limit:page*limit])
     resp ={
         "code": 0
         , "msg": ""
-        , "count": len(person_list)
-        , "data":person_list[(page-1)*limit:page*limit]
+        , "count": len(n_list)
+        , "data":n_list[(page-1)*limit:page*limit]
     }
     print '【VIP人员接口数据】：'+json.dumps(resp)
 
     return HttpResponse(json.dumps(resp), content_type="application/json")
+
+def add_vip_person(req):
+    print req
+    print req.POST
+    data = req.POST.copy()
+    if data['sex'] == '男':
+        data['sex'] = '1'
+    else:
+        data['sex'] = '0'
+
+    db = Mysql()
+    sql = "INSERT INTO person (`name`, `sex`, `phone`, `note`, `create_time`, `resrver1`)" \
+          " VALUES('%s','%s','%s','%s',now(),NULL)"%(data['username'],data['sex'],data['phone'],data['desc'])
+    print sql
+    dd = db.insertOne(sql)
+    db.dispose()
+    if dd == 0:
+        resp = {
+            "code": 1
+            , "msg": "success"
+        }
+        return HttpResponse(json.dumps(resp), content_type="application/json")
+    else:
+        resp = {
+            "code": 0
+            , "msg": "success"
+        }
+        return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+
+
 
